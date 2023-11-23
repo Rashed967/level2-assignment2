dfb8cd33c9fc2e3dea6488ee33f8851c86c77ce3
Rashed967 <rashedhasan967@gmail.com>
2023-11-23 10:40:59 +0600

    just opened the repo

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+import app from './app';
+import config from './config';
+
+async function main() {
+  try {
+    await mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+    // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+    app.listen(config.port, () => {
+      console.log(`server is running on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+main();
